587c0be97a0f64632bb6136106e4c4ada459449e
SaQr AlGhamdi <h7lc0n@gmail.com>
2021-11-06 22:33:21 +0300

    tools/getroot.sh file updated - date/time removed

--- tools/getroot.sh
+++ tools/getroot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/usr/bin/bash
 
+
 if [ "$EUID" -ne 0 ]; then
-    date +"%F %H:%M:%S"
     echo -e "\t[W] Permission Denied"
     echo -e "\t[i] Please run as root ..."
     exit 69     #-- Pretty error code, aint it ?😏
